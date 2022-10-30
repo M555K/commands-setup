c8a6fa46f1bb516fa6a87f972e1bc2ab56cc9414
Maria <kitmaria.2015@gmail.com>
2022-10-30 23:13:00 +0100

    fixFormat() and camelCase() are added

--- src/utilities/BrowserUtils.java
+++ src/utilities/BrowserUtils.java
@@ -1,4 +1,31 @@
 package utilities;
 
 public class BrowserUtils {
+    public static String reverse(String str){
+        String reverse = "";
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+    public static String fixFormat(String str){
+        return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+
+    public static String camelCase(String str){
+
+        String[] allWords = str.split(" ");
+
+        String camelCase = allWords[0].toLowerCase();
+
+        for(int i = 1; i < allWords.length; i++){ // I'm starting from index 1 because I already used the first word
+            camelCase += fixFormat(allWords[i]);
+        }
+
+        return camelCase;
+
+    }
+
 }
